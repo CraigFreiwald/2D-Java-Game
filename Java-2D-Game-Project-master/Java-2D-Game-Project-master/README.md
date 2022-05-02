75bc133aabb5742e2b2eb4bec7d053aa3c8a7f29
CraigFreiwald <98433413+CraigFreiwald@users.noreply.github.com>
2022-05-02 09:11:17 -0400

    Add files via upload
    Initial loading of project files in to repository.

---
+++ Java-2D-Game-Project-master/Java-2D-Game-Project-master/README.md
@@ -0,0 +1,2 @@
+# Java-2D-Game-Project
+Java 2D Game Project to create a mario-like game
---
+++ Java-2D-Game-Project-master/Java-2D-Game-Project-master/src/main/java/Main.java
@@ -0,0 +1,10 @@
+import Jade.Window;
+
+//Main class to run application
+public class Main {
+    public static void main(String[] args) {
+
+        Window window = Window.get();
+        window.run();
+    }
+}
